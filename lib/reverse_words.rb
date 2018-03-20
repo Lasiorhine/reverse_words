5487ed280c0a21cd17e9dc1918d199cfc5cb15ac
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-03-19 18:43:39 -0700

    passing all tests

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -5,31 +5,34 @@ require 'pry'
 def reverse_words(my_words)
   index = 0
   temp_array = []
-  (my_words.length + 1).times do
-    if my_words[index] != " "
-      temp_array << my_words[index]
-    elsif my_words[index] == " " || index == (my_words.length + 1)
-      temp_origin = (temp_array.length - 1)
-      temp_target = 0
-      temp_temp = nil
-      (temp_array.length / 2).times do
-        temp_temp = temp_array[temp_target]
-        temp_array[temp_target] = temp_array[temp_origin]
-        temp_array[temp_origin] = temp_temp
-        temp_origin -= 1
-        temp_target += 1
+  if my_words == nil || my_words.length <= 1
+    return my_words
+  else
+    (my_words.length + 1).times do
+      if my_words[index] == " " || index == (my_words.length)
+        temp_origin = (temp_array.length - 1)
+        temp_target = 0
+        temp_temp = nil
+        (temp_array.length / 2).times do
+          temp_temp = temp_array[temp_target]
+          temp_array[temp_target] = temp_array[temp_origin]
+          temp_array[temp_origin] = temp_temp
+          temp_origin -= 1
+          temp_target += 1
+        end
+        replacement_index = index - 1
+        origin_index = (temp_array.length - 1)
+        (temp_array.length).times do
+          my_words[replacement_index] = temp_array[origin_index]
+          replacement_index -= 1
+          origin_index -= 1
+        end
+        temp_array = []
+      else
+        temp_array << my_words[index]
       end
-      replacement_index = index - 1
-      origin_index = (temp_array.length - 1)
-      (temp_array.length).times do
-        my_words[replacement_index] = temp_array[origin_index]
-        replacement_index -= 1
-        origin_index -= 1
-      end
-      temp_array = []
+      index += 1
     end
-    index += 1
   end
   return my_words
-  # raise NotImplementedError
 end
